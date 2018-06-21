fc18200d0e51a7fa2c81104ed6c1f59542049cba
hwbaker <hewei860311@sohu.com>
2018-06-21 14:49:30 +0800

    VIEWS

--- app/views/warehouse.php
+++ app/views/warehouse.php
@@ -1,22 +1,37 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>Warehouse</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+</head>
+
+<body >
+
+<br>
 <table width="100%" border="1px">
     <tr>
         <td>仓库标识</td>
         <td>仓库</td>
     </tr>
-<?php
-/**
- * Created by PhpStorm.
- * User: hwbaker
- * Date: 2018/5/29
- * Time: 14:40
- */
 
-foreach ($ware as $item) {
-    echo "<tr>";
-    echo "<td>{$item['warehouse_id']}</td>";
-    echo "<td>{$item['name']}</td>";
-    echo "</tr>";
-}
-?>
+    <?php
+
+    foreach ($ware as $item) {
+        echo "<tr>";
+        echo "<td>{$item['warehouse_id']}</td>";
+        echo "<td>{$item['name']}</td>";
+        echo "</tr>";
+    }
+    ?>
 
 </table>
+
+</body>
+</html>
+
+
+
+
